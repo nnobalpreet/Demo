ef7fba41c546a766a9eeecfc3b587e085992a30b
Nobalpreet Nobalpreet <39172933+nnobalpreet@users.noreply.github.com>
2019-08-30 13:22:35 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "e0c31db0-9833-40f8-920e-e58fd0438a1c",
+                  Analytics.class, Crashes.class);
     }
 }
